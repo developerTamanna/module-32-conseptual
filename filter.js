2a0abdd11a93d7bdbdbd9a868fe05bf674518cb4
developerTamanna <astamanna57@gmail.com>
2025-03-09 10:13:32 +0600

    filter odd and even

---
+++ filter.js
@@ -0,0 +1,14 @@
+const number =[2,3,56,78,44,5,9,22];
+const evenNumber = number.filter(number=>{
+
+        return number % 2===0;
+    
+})
+// console.log(evenNumber)
+
+
+//?odd number
+
+const numbers =[2,3,56,78,44,5,9,22];
+const oddNumber = number.filter(num=> num %2 !==0)
+console.log(oddNumber)
