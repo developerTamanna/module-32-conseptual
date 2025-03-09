9a1efad7834a1e02dcf17e9538d9ed9780b935ca
developerTamanna <astamanna57@gmail.com>
2025-03-09 11:44:28 +0600

    operator

---
+++ operator.js
@@ -0,0 +1,15 @@
+//?operators
+
+//*logical and operator
+
+// const age = 15;
+// const hasLicense = true;
+// if(age> 18 && hasLicense){
+//     console.log('you can drive')
+// }
+
+const age = 15;
+const hasLicense = true;
+if(age> 18 || hasLicense){
+    console.log('you can drive')
+}
