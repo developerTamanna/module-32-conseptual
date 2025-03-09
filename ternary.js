3b54ebf056575215cccaf737fe0efc91bee207aa
developerTamanna <astamanna57@gmail.com>
2025-03-09 11:54:03 +0600

    ternary

---
+++ ternary.js
@@ -0,0 +1,6 @@
+// ? ternary(?)
+
+//* condition ? print this =true : print this = false
+
+const hasLicense = false;
+hasLicense===true ? console.log('you can drive') : console.log('you cant drive')
