339cdfc29f557fa0ffa83bfe1fc357a26fcc8b50
developerTamanna <astamanna57@gmail.com>
2025-03-09 11:25:24 +0600

    reduce array

---
+++ reduce.js
@@ -0,0 +1,29 @@
+// const numbers =[2,3,5,6,4,98,2];
+// const total = numbers.reduce((acce, current)=>{
+//    return acce + current; 
+// },0);
+// console.log(total)
+
+
+//? next: reduce 
+
+//* reduce er kaj 
+
+// const numbers =[4,5,6,23,4,5,6,2];
+// const total = numbers.reduce((acc, number)=>{
+//      return acc + number;
+// },0)
+// console.log(total)
+
+
+
+//*next:
+//?reduce array
+
+const array = [[2, 3], [3, 4], [6, 7]];
+const singleArray = array.reduce((acc, current)=>{
+ return acc.concat(current)
+},[]);
+console.log(singleArray)
+
+
